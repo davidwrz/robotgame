d462a78322b9200768235867ce116091249d3ebc
Dawid Wrzos <wrzosdawid@wp.pl>
2023-12-03 23:51:31 +1100

    Refactor

--- src/test/java/au/entsia/game/GameTest.java
+++ src/test/java/au/entsia/game/GameTest.java
@@ -38,6 +38,59 @@ class GameTest {
     }
 
     @Test
+    void shouldNotPlaceRobotWhenPlaceInvalid() {
+        // given
+        // when
+        when(inputReader.getInput())
+                .thenReturn("PLACE -1,1,NORTH")
+                .thenReturn("EXIT");
+
+        when(robot.getHorizontalCoordinate()).thenReturn(-1);
+        when(robot.getVerticalCoordinate()).thenReturn(1);
+        when(robot.getFacing()).thenReturn("NORTH");
+        game.placeRobot("PLACE -1,1,NORTH");
+
+        // then
+        verify(robot,never()).place(anyInt(), anyInt(), anyString());
+    }
+
+    @Test
+    void shouldMoveRobotWhenTriggeredMoveInGame() {
+        // given
+        // when
+        when(inputReader.getInput())
+                .thenReturn("PLACE 1,1,NORTH")
+                .thenReturn("MOVE")
+                .thenReturn("EXIT");
+
+        when(robot.getHorizontalCoordinate()).thenReturn(1);
+        when(robot.getVerticalCoordinate()).thenReturn(1);
+        when(robot.getFacing()).thenReturn("NORTH");
+        game.moveRobot();
+
+        // then
+        verify(robot).move();
+    }
+
+    @Test
+    void shouldNotMoveRobotWhenPlaceInvalid() {
+        // given
+        // when
+        when(inputReader.getInput())
+                .thenReturn("PLACE 0,0,SOUTH")
+                .thenReturn("MOVE")
+                .thenReturn("EXIT");
+
+        when(robot.getHorizontalCoordinate()).thenReturn(0);
+        when(robot.getVerticalCoordinate()).thenReturn(0);
+        when(robot.getFacing()).thenReturn("SOUTH");
+        game.moveRobot();
+
+        // then
+        verify(robot, never()).move();
+    }
+
+    @Test
     void shouldRotateLeftRobotWhenTriggeredRotateInGame() {
         // given
         // when
@@ -71,41 +124,5 @@ class GameTest {
 
         // then
         verify(robot).rotateRight();
-    }
-
-    @Test
-    void shouldMoveRobotWhenTriggeredMoveInGame() {
-        // given
-        // when
-        when(inputReader.getInput())
-                .thenReturn("PLACE 1,1,NORTH")
-                .thenReturn("MOVE")
-                .thenReturn("EXIT");
-
-        when(robot.getHorizontalCoordinate()).thenReturn(1);
-        when(robot.getVerticalCoordinate()).thenReturn(1);
-        when(robot.getFacing()).thenReturn("NORTH");
-        game.moveRobot();
-
-        // then
-        verify(robot).move();
-    }
-
-    @Test
-    void shouldNotMoveRobotWhenPositionNotValid() {
-        // given
-        // when
-        when(inputReader.getInput())
-                .thenReturn("PLACE 0,0,SOUTH")
-                .thenReturn("MOVE")
-                .thenReturn("EXIT");
-
-        when(robot.getHorizontalCoordinate()).thenReturn(0);
-        when(robot.getVerticalCoordinate()).thenReturn(0);
-        when(robot.getFacing()).thenReturn("SOUTH");
-        game.moveRobot();
-
-        // then
-        verify(robot, never()).move();
     }
 }
